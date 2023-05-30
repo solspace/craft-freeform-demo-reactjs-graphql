4fe1792b022ad3afd257e0eeb96bef01af95c10c
Sean Delaney <sean@solspace.com>
2023-05-30 11:32:19 +0100

    Basic demo setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Form from './Form';
+
+function App() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h2 className="mb-3 text-2xl font-semibold">ReactJS + GraphQL demo for Freeform 4.x for Craft</h2>
+      <Form />
+    </main>
+  );
+}
+
+export default App;
