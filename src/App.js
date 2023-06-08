30f002cbe3008aecc0ad7c9ae3a9b60ed7ecd0dd
Sean Delaney <sean@solspace.com>
2023-06-08 20:41:51 +0100

    Connected with Solspace demo site

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import './App.css';
-import Form from './Form';
+import ContactForm from './ContactForm';
 
 function App() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <h2 className="mb-3 text-2xl font-semibold">ReactJS + GraphQL demo for Freeform 4.x for Craft</h2>
-      <Form />
+      <ContactForm />
     </main>
   );
 }
