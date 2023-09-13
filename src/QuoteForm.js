5b984cb669122ffc0a9e59cd2b74f656ae222246
Sean Delaney <sean@solspace.com>
2023-09-13 13:18:43 +0100

    Messaging Updates

--- src/QuoteForm.js
+++ src/QuoteForm.js
@@ -33,9 +33,13 @@ const defaultFormProperties = {
         handle: '',
         name: '',
     },
-    loadingText: '',
-    successMessage: '',
-    errorMessage: '',
+    settings: {
+        behavior: {
+            processingText: '',
+            successMessage: '',
+            errorMessage: '',
+        },
+    },
 };
 
 const RECAPTCHA_SITE_KEY = '6Lce6nQmAAAAAO5d4LWC6TkECxNRSG7WNiVj17B1';
@@ -143,7 +147,7 @@ const Form = () => {
 
     const startProcessing = () => {
         submitButton.style.cursor = 'not-allowed';
-        submitButton.innerText = formProperties.loadingText;
+        submitButton.innerText = formProperties.settings.behavior.processingText;
     };
 
     const stopProcessing = () => {
@@ -286,10 +290,10 @@ const Form = () => {
         <form className="text-center flex flex-col items-left justify-left" onSubmit={handleSubmit}>
             <h3 className="mb-4 text-xl font-normal text-left">Quote Form</h3>
             <div id="successMessage" className="w-full bg-green-100 border border-green-400 text-sm text-left text-green-700 px-4 py-2 rounded-md mb-8" style={{ display: 'none' }}>
-                <p>{formProperties.successMessage}</p>
+                <p>{formProperties.settings.behavior.successMessage}</p>
             </div>
             <div id="errorMessage" className="w-full bg-red-100 border border-red-400 text-sm text-left text-red-500 px-4 py-2 rounded-md mb-8" style={{ display: 'none' }}>
-                <p>{formProperties.errorMessage}</p>
+                <p>{formProperties.settings.behavior.errorMessage}</p>
             </div>
             <div className="flex flex-col w-full space-y-3">
                 <div className="form-row">
